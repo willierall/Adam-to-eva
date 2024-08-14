5dee2e3bfaf26f8b8270074be50bda7a4fef0b7c
Testing App <willie@andilesolutions.com>
2024-08-14 16:01:05 +0200

    New DB credentials

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_HOST', 'terraform-20240814101940372500000001.ckpdjdgeysia.eu-central-1.rds.amazonaws.com:3306');
-define('DB_NAME', 'ClientRelationStoreDB');
+define('DB_NAME', 'AdamToEva');
 define('DB_USER', 'admin');
-define('DB_PASS', 'Chang3m3');
+define('DB_PASS', 'yourpassword');
 
 define('OPENSEARCH_ENDPOINT', 'your-opensearch-endpoint');
 define('OPENSEARCH_PORT', 443);
